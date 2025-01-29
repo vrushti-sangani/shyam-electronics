d6175daeaf1dffb5f67bca818f273d4ae57d6d8e
vrushti-sangani <130216959+vrushti-sangani@users.noreply.github.com>
2025-01-29 16:19:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// change img
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000); // Change image every 3 seconds
